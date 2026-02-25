643628887553dfdf50105d48c9288c76d7e3a157
Jono <jono1231@cs.washington.edu>
2026-02-24 20:09:27 -0800

    damn i forgor that i renamed be-a-legend

--- web/be-a-legend/solve.py
+++ web/be-a-legend/solve.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python3
 """
-Solve script for Dragon's Lair CTF challenge
-
-The vulnerability: When you SAVE and then LOAD during combat, the game uses
-__dict__.update() which modifies the existing Player object in-place. This means
-the combat loop continues running with the same object reference, but we can
-restore our HP mid-fight by loading a save from before we took damage.
-
 Strategy:
 1. SAVE at full HP
 2. Start FIGHT
@@ -26,15 +19,15 @@ async def exploit():
         msg = await ws.recv()
         print(msg)
         
-        print("\n[+] Saving game at full HP...")
+        print("\nSaving game at full HP...")
         await ws.send("SAVE")
         msg = await ws.recv()
         print(msg)
         
-        print("[+] Starting combat...")
+        print("Starting combat...")
         await ws.send("FIGHT")
         
-        print("[+] Spamming LOAD to restore HP during combat...")
+        print("Spamming LOAD to restore HP during combat...")
         
         try:
             while True:
@@ -67,7 +60,7 @@ async def exploit():
                 await asyncio.sleep(0.05)
                 
         except KeyboardInterrupt:
-            print("\n[!] Interrupted by user")
+            print("\nInterrupted by user")
 
 if __name__ == "__main__":
     asyncio.run(exploit())
